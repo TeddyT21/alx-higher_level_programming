8297a6afec3b27551ee646947be94a5de9496471
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2023-01-11 08:25:32 +0300

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except KeyboardInterrupt:
+        except ValueError:
             result = b + a
             break
     return (result)
