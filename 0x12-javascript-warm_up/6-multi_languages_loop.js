a42a53bd9bcaaee6135b4c5214fdd0bdbef2cdb6
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2023-03-13 15:55:07 +0300

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by
-// using an array of string and a loop
-
-const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(array[i]);
 }
