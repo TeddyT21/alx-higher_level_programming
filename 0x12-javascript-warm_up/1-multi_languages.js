b2c66c68c3919b7575f8161162fc1f7373a08f3a
Tewodros Tesfaye <tmt2164@gmail.com>
2023-03-13 05:18:18 -0700

    Proj-js.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Print string "Javascript is amazing"
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// searches the second biggest integer in the list of arguments.
+
+const arg = process.argv;
+if (arg.length < 4) {
+  console.log('0');
+} else {
+  const nums = [];
+  for (let i = 2; i < arg.length; i++) {
+    nums.push(parseInt(arg[i]));
+  }
+  const largest = Math.max(...nums);
+  nums.splice(nums.indexOf(largest), 1);
+  console.log(Math.max(...nums));
+}
