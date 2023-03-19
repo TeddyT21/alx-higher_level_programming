ac8f898fb84e7d786d831076b553ee2ea6a58554
TeddyT21 <111092662+TeddyT21@users.noreply.github.com>
2023-03-19 22:19:58 +0300

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """List all states from a given db sorted in ascending order by id
-Username, password, and database names are given as user args
 """
-import sys
+
 import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
@@ -20,3 +20,7 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
+
+  """
+  db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+  """
